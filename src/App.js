9684af442cac88fe67cbc9dc3f33b9b90f54086e
divya patidar <patidardivya016@gmail.com>
2024-09-29 19:02:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import logo from './logo.svg' 
+import './index.css'
+
+import Newsapp from './component/Newsapp'
+
+   function App(){
+            
+             return(
+                  <>
+               
+                 <Newsapp/> 
+                 
+                  </>
+              
+       )
+       }
+              export default App
+   
+
+ 
+  
+ 
+ 
+
+
